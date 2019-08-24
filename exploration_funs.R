251a83edfb821fddd4aa2bf65d4247d389ab8d2a
vogelweide1985 <vogelweide1985@hotmail.de>
2019-08-24 20:35:08 +0200

    #Updated metric_metric

--- exploration_funs.R
+++ exploration_funs.R
@@ -23,12 +23,11 @@ iplot_univar_metric <- function(x) {
 iplot_bivar_metric_metric <- function(x, y) {
    #TO DO:  p4
    p1 <- plot_ly(x = ~x, y = ~y, type = 'scatter', mode = "markers")
-   p2 <- plot_ly(x = x, type = "histogram") %>%
+   p2 <- plot_ly(x= x, y = y, type = "histogram2dcontour")
+   p3 <- plot_ly(x = x, type = "histogram", alpha = 0.6) %>%
       add_histogram(x = y) %>%
       layout(barmode = "overlay")
-   p3 <- plot_ly(x= x, y = y, type = "histogram2dcontour")
-   p4 <- plot_ly(y = x, type = "violin")
    
-   subplot(p1, p2, p3, p4, nrows = 2)
+   subplot(p1, p2, p3, nrows = 2)
    
 }
--- main.R
+++ main.R
@@ -6,19 +6,28 @@ source("exploration_funs.R")
 df <- iris
 x <- df$Sepal.Length
 y <- df$Sepal.Width
+z <- df$Species
 
 plot_univar_metric(df$Sepal.Length)
 plot_univar_metric(df$Sepal.Width)
 plot_univar_metric(df$Petal.Length)
 plot_univar_metric(df$Petal.Width)
 
-
-
 iplot_univar_metric(df$Sepal.Length)
 iplot_univar_metric(df$Sepal.Width)
 iplot_univar_metric(df$Petal.Length)
 iplot_univar_metric(df$Petal.Width)
 
+iplot_bivar_metric_metric(df$Sepal.Length, df$Sepal.Width)
+iplot_bivar_metric_metric(df$Sepal.Length, df$Petal.Length)
+iplot_bivar_metric_metric(df$Sepal.Length, df$Petal.Width)
+iplot_bivar_metric_metric(df$Sepal.Width, df$Petal.Length)
+iplot_bivar_metric_metric(df$Sepal.Width, df$Petal.Width)
+iplot_bivar_metric_metric(df$Petal.Length, df$Petal.Width)
+
 
 iplot_bivar_metric_metric(x, y)
+iplot_bivar_metric_metric(x, y)
+
+
 
